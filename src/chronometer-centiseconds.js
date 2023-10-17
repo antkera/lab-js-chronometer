7d5b7e24d87c3a41968f6d5681f001f5787b7f98
antkera <128487289+antkera@users.noreply.github.com>
2023-10-17 19:33:44 +0200

    terminado

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 10);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 6000);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 100) % 60;
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return this.currentTime % 100;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let str = "0";
+    let val = value.toString();
+    if (value > 9) {
+      return val;
+    } else {
+      return str + val;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let sec = this.computeTwoDigitNumber(this.getSeconds());
+    let min = this.computeTwoDigitNumber(this.getMinutes());
+    let cs = this.computeTwoDigitNumber(this.getCentiseconds());
+    return `${min}:${sec}.${cs}`;
   }
 }
